141c3f440c23ae4a3dcea81fc87411cdfd1b289a
u7chan <34462401+u7chan@users.noreply.github.com>
2024-07-08 23:48:56 +0900

    fix: build ok

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from 'next-auth'
 import Image from 'next/image'
 import Link from 'next/link'
-import { authOptions } from '../api/auth/[...nextauth]/route'
+import { authOptions } from '../api/auth/[...nextauth]/authOptions'
 
 export default async function Page() {
   // getServerSession の authOptions を設定すると、authOptions 内の session コールバックが呼ばれるようになる
